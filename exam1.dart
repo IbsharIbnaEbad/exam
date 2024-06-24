7977855203012f0158d6ad237a7501386b23defc
ibshar <cse_182210012101018@lus.ac.bd>
2024-06-24 23:10:57 +0600

    ans 1

--- exam1.dart
+++ exam1.dart
@@ -1,3 +1,22 @@
-main(){
-  print('hello');
+class Media {
+  void play() {
+    print("Playing media");
+  }
+}
+
+class Song extends Media {
+  String artist = 'devda';
+
+  @override
+  play() {
+    print("Playing song by $artist");
+  }
+}
+
+void main() {
+  Media basanti = Media();
+  Media basanti2 = Song();
+
+  basanti.play();
+  basanti2.play();
 }
